3ffa6ac6a9d50d28335d9da99a52aacec296a16c
Ruslan Ovsyannikov <ovsyannikov@helmholtz-berlin.de>
2022-06-16 05:06:34 +0000

    Implemented MappedDict and rewritten SafeValueDict

--- RayPyNG/collections.py
+++ RayPyNG/collections.py
@@ -2,8 +2,12 @@
 ###############################################################################
 # Generic superclasses for collections line dict() or list()
 
-from collections import UserDict
+from collections import UserDict, UserList
 import keyword
+
+from collections.abc import MutableMapping,MutableSequence
+
+import typing
 
 ###############################################################################
 def protectName(name:str)->str:
@@ -31,31 +35,99 @@ def protectName(name:str)->str:
 
 
 ###############################################################################
+# based on the collections.UserDict implementation from python 3.8.10
+class MappedDict(MutableMapping):
+    """MappedDict class implements a dictionary which will apply the given 
+    function to each value added to it and also will keep a track of original 
+    values.
+    """
+    def __init__(self, func:typing.Callable=None,dict=None, **kwargs):
+        """Initializes instance of MappedDict class
 
-###############################################################################
-class SafeValueDict(UserDict):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._original_values = dict()
+        Args:
+            func (typing.Callable, optional): function to be called on each
+                new value added to the dictionary. Defaults to None.
+            dict (optional): Optional initilizer, any dict-like object. 
+                Defaults to None.
 
-    def __setitem__(self, key, item) -> None:
-        self._original_values[key]=item
-        return super().__setitem__(key, protectName(item))
+        Raises:
+            TypeError: if suppled 'func' is not callable
+        """
+        if func is None:
+            func = lambda x: x
+        elif not callable(func):
+            raise TypeError("callback parameter of 'MappedDict' object must be callable")
+        self.__func = func        
+        self.__data = {}
+        self.__original_data = {}
+
+        if dict is not None:
+            self.update(dict)
+        if kwargs:
+            self.update(kwargs)
 
     def original(self):
-        return self._original_values
+        """prodives acccess to the original, unmodified data
 
-    # # Function to stop deletion
-    # # from dictionary
-    # def __del__(self):
-    #     raise RuntimeError("Deletion not allowed")
-         
-    # # Function to stop pop from
-    # # dictionary
-    # def pop(self, s = None):
-    #     raise RuntimeError("Deletion not allowed")
-         
-    # # Function to stop popitem
-    # # from Dictionary
-    # def popitem(self, s = None):
-    #     raise RuntimeError("Deletion not allowed")
+        Returns:
+            dict: dictionary with original data
+        """
+        return self.__original_data
+
+    def __len__(self): return len(self.__data)
+    def __getitem__(self, key):
+        if key in self.__data:
+            return self.__data[key]
+        if hasattr(self.__class__, "__missing__"):
+            return self.__class__.__missing__(self, key)
+        raise KeyError(key)
+    def __setitem__(self, key, item):
+        self.__original_data[key]=item
+        self.__data[key] = self.__func(item)
+    def __delitem__(self, key): 
+        del self.__original_data[key]
+        del self.__data[key]
+    def __iter__(self):
+        return iter(self.__data)
+
+    # Modify __contains__ to work correctly when __missing__ is present
+    def __contains__(self, key):
+        return key in self.__data
+
+    # Now, add the methods in dicts but not in MutableMapping
+    def __repr__(self): return repr(self.__data)
+    def __copy__(self):
+        inst = self.__class__.__new__(self.__class__)
+        inst.__dict__.update(self.__dict__)
+        # Create a copy and avoid triggering descriptors
+        inst.__dict__["__data"] = self.__dict__["__data"].copy()
+        inst.__dict__["__original_data"] = self.__dict__["__original_data"].copy()
+        inst.__dict__["__callback"] = self.__dict__["__callback"].copy()
+        return inst
+
+    def copy(self):
+        if self.__class__ is MappedDict:
+            return MappedDict(self.__original_data.copy())
+        import copy
+        data = self.__data
+        try:
+            self.__data = {}
+            c = copy.copy(self)
+        finally:
+            self.__data = data
+        c.update(self)
+        return c
+
+    @classmethod
+    def fromkeys(cls, iterable, value=None):
+        d = cls()
+        for key in iterable:
+            d[key] = value
+        return d
+
+
+###############################################################################
+class SafeValueDict(MappedDict):
+    def __init__(self, dict=None, **kwargs):
+        super().__init__(protectName, dict, **kwargs)
+
