85abfe1ba8ee2f4d246bef8d52e1a2105a062961
Ruslan Ovsyannikov <ovsyannikov@helmholtz-berlin.de>
2022-06-17 06:35:09 +0000

    updated test.py for the new directory structure

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ a.load("test")
 
 from RayPyNG.rml import RMLFile
 
-rml1 = RMLFile('tests/rml1.xml',template='rml/beamline.rml')
-rml2 = RMLFile('tests/rml2.xml',template='tests/rml/high_energy_branch_flux_1200.rml')
+rml1 = RMLFile('local/rml1.xml',template='rml/beamline.rml')
+rml2 = RMLFile('local/rml2.xml',template='examples/rml/high_energy_branch_flux_1200.rml')
 
 rml1.write()
 rml2.write()
