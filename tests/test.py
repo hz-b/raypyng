07d47b69fc21786b77a632ba5625bfa0d3a562a1
Ruslan Ovsyannikov <ovsyannikov@helmholtz-berlin.de>
2022-06-17 06:35:34 +0000

    moved all tests into the tests/ directory

--- tests/test.py
+++ tests/test.py
@@ -12,7 +12,8 @@ a.load("test")
 from RayPyNG.rml import RMLFile
 
 rml1 = RMLFile('local/rml1.xml',template='rml/beamline.rml')
-rml2 = RMLFile('local/rml2.xml',template='examples/rml/high_energy_branch_flux_1200.rml')
+rml2 = RMLFile('local/rml2.xml',template='example0
+s/rml/high_energy_branch_flux_1200.rml')
 
 rml1.write()
 rml2.write()
