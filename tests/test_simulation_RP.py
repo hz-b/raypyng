5f566355933011dbd0889bf38625ed57ec049636
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2022-07-22 17:59:37 +0200

    add RP simulation function, + correct a bug that discard different objects with same parameter

--- tests/test_simulation_RP.py
+++ tests/test_simulation_RP.py
@@ -9,10 +9,13 @@ sim = Simulate('examples/rml/high_energy_branch_flux_1200.rml',template='example
 rml=sim.rml
 elisa = sim.rml.beamline
 
+energy_range = np.arange(1000,2001,500)
+cff = {elisa.PG.cFactor:2.5}
+ES = {elisa.ExitSlit.totalHeight:[0.1,0.05]}
 
-
+sim.analyze = False
 # test resolving power simulations
-sim._RP_simulation(elisa.Dipole, np.arange(1000,1101,50), elisa.DetectorAtFocus)
+sim.RP_simulation(energy_range, elisa.DetectorAtFocus,ES,cff,cpu=5, force=False)
 
 # to run the script
 # PYTHONPATH=. python tests/test_simulate_RP.py
