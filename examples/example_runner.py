e612b92db28e79060fd1a0810fc1eac1662a9071
jny <simone.vadilonga@helmholtz-berlin.de>
2022-10-17 08:27:06 +0200

    add runner example

---
+++ examples/example_runner.py
@@ -0,0 +1,39 @@
+import os
+import time
+import os
+
+from raypyng.runner import RayUIRunner, RayUIAPI
+
+
+r = RayUIRunner(ray_path=None, hide=True)
+a = RayUIAPI(r)
+
+
+# Start a RAY-UI instance
+r.run()
+
+# Confirm that it is running and print the process id
+print('Confirm that RAY-UI is running:', r.isrunning)
+print("RAY-UI is running with pid", r.pid)
+
+# load an rml file
+print("Loading rml file")
+a.load('rml/elisa.rml')
+
+print("Trace...")
+a.trace()
+
+print("Exporting")
+this_file_dir=os.path.dirname(os.path.realpath(__file__))
+a.export("Dipole,DetectorAtFocus", "RawRaysOutgoing", this_file_dir, 'test_export')
+
+
+print("Killing the RAY-UI process")
+r.kill()
+time.sleep(2)
+print('Confirm that RAY-UI is running:', r.isrunning)
+
+
+
+
+        
