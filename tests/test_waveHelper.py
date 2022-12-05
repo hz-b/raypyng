8288a07c90d69a9bfe1d511921c7fb2cc575521e
jny <simone.vadilonga@helmholtz-berlin.de>
2022-12-05 12:48:46 +0100

    add printout of the paths

--- tests/test_waveHelper.py
+++ tests/test_waveHelper.py
@@ -12,7 +12,9 @@ v.report_available_energies(verbose=True)
 matching_en = np.arange(80,570,10)
 matchin_en_list = v.convert_energies_to_file_list(1,matching_en)
 
+for ind, en in enumerate(matching_en):
+    print(f"Energy: {en}, filename: {matchin_en_list[ind]}")
+
 non_matching_en = np.arange(81,570,10)
 non_matchin_en_list = v.convert_energies_to_file_list(1,non_matching_en)
 
-# print(f"matchin_en_list {matchin_en_list}")
