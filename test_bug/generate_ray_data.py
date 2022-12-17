3e99200503d2df204eb54910e369699cc1d87b57
jny <simone.vadilonga@helmholtz-berlin.de>
2022-12-17 08:06:34 +0100

    add correct/buggy version and instructions to test killed problem

--- test_bug/generate_ray_data.py
+++ test_bug/generate_ray_data.py
@@ -9,8 +9,25 @@ sim      = Simulate(rml_file, hide=True)
 
 sim.analyze = False
 
-nsim = 1e3
-myRecipe = ML(nsim,['DetectorAtFocus'],sim_folder='ELISA_'+"{:.0e}".format(nsim))
+nsim = 1e1
+
+# correct output using a dirty trick:
+myRecipe = ML(nsim,['DetectorAtFocus'],sim_folder='ELISA_correct', dirty_hack=True)
+
+# buggy output using a dirty trick:
+myRecipe = ML(nsim,['DetectorAtFocus'],sim_folder='ELISA_bug', dirty_hack=False)
 
 # test resolving power simulations
 sim.run(myRecipe, multiprocessing=4, force=False)
+
+
+############################################
+# to reproduce the problem depends on the specs of your computer, i guess. 
+# In the following I increase the number of simulations, and I use all the cpus. 
+# Hopefully that should work and produce the "killed" output
+
+
+# myRecipe = ML(1e6,['DetectorAtFocus'],sim_folder='ELISA_bug', dirty_hack=True)
+
+# # test resolving power simulations
+# sim.run(myRecipe, multiprocessing=True, force=False)
