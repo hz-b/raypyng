45a3735b0194935db583991a6e1ff4946302064a
simone.vadilonga1 <simone.vadilonga@gmail.com>
2024-03-04 23:35:40 +0100

    typo

--- examples/example_waveHelper.py
+++ examples/example_waveHelper.py
@@ -9,7 +9,7 @@ WH.report_available_energies(verbose=True)
 energies = np.arange(80,570,10)
 matchin_en_list = WH.convert_energies_to_file_list(1,energies)
 
-for ind, en in enumerate(matching_en):
+for ind, en in enumerate(matchin_en_list):
     print(f"Energy: {en}, filename:{matchin_en_list[ind]}")
 
 
