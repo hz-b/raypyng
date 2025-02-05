a85dbf0ba06020ff9ee9bf405af9975dadc356cb
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-02-05 10:21:29 +0100

    add undulator table to the example

--- examples/example_simulation_noanalyze_undulator.py
+++ examples/example_simulation_noanalyze_undulator.py
@@ -1,12 +1,11 @@
 from raypyng import Simulate
 import numpy as np
 import os
-import time
+import pandas as pd
 
-st = time.time()
 
 this_file_dir=os.path.dirname(os.path.realpath(__file__))
-rml_file = os.path.join(this_file_dir,'rml/simple_undulator_beamline.rml')
+rml_file = os.path.join(this_file_dir, 'rml', 'simple_undulator_beamline.rml')
 
 sim = Simulate(rml_file, hide=True)
 
@@ -16,10 +15,10 @@ beamline = sim.rml.beamline
 
 
 # define the values of the parameters to scan 
-energy    = np.arange(200, 2201,200)
+energy    = np.arange(200, 2201,250)
 SlitSize  = np.array([0.1])
 cff       = np.array([2.25])
-nrays     = 1e5
+nrays     = 1e4
 
 # define a list of dictionaries with the parameters to scan
 params = [  
@@ -39,7 +38,7 @@ sim.params=params
 sim.simulation_name = 'test_noAnalyze_undulator'
 
 # repeat the simulations as many time as needed
-sim.repeat = 1
+sim.repeat = 2
 
 sim.analyze = False # don't let RAY-UI analyze the results
 sim.raypyng_analysis=True # let raypyng analyze the results
@@ -49,15 +48,14 @@ sim.exports  =  [{beamline.SU:['RawRaysOutgoing']},
                 {beamline.DetectorAtFocus:['RawRaysOutgoing']}
                 ]
 
+undulator_file_path = os.path.join(this_file_dir, 
+                                   'undulator', 
+                                   'undulator_harmonics_energy_photons.csv')
+
+undulator = pd.read_csv(undulator_file_path)
+
+sim.undulator_table=undulator
 #uncomment to run the simulations
-sim.run(multiprocessing=1, force=True)
+sim.run(multiprocessing=6, force=True)
 
 
-
-time_needed = time.time() - st
-
-# Convert the time difference to minutes and seconds
-minutes, seconds = divmod(time_needed, 60)
-
-# Print the time needed in minutes:seconds format
-print(f"Time needed: {int(minutes)}:{int(seconds):02d}")
