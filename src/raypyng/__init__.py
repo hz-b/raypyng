88e9acb7662c18cacc9b298f85f9e7637a2b2c8a
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-06-23 09:31:48 +0200

    update docs and toml

--- src/raypyng/__init__.py
+++ src/raypyng/__init__.py
@@ -1,4 +1,5 @@
+from .dipole_flux import Dipole
 from .rml import RMLFile
 from .simulate import Simulate, SimulationParams
 
-__all__ = ["RMLFile", "Simulate", "SimulationParams"]
+__all__ = ["RMLFile", "Simulate", "SimulationParams", "Dipole"]
