b01926bf2bbe454c83c061abfb0172c3e36c683a
Simone Vadilonga <simone.vadilonga@helmholtz-berlin.de>
2025-11-25 15:07:05 +0100

    patch for MacOs

--- src/raypyng/config.py
+++ src/raypyng/config.py
@@ -1,9 +1,15 @@
 # this is shared configuration space
+import os
 
 """ray_path contains default path to the ray installation
 """
 ray_path = None
 
 """ray_binary contains name of the default binary or shell script
+   depending on the operating system
 """
-ray_binary = "rayui.sh"
+opsys = os.popen("uname").read().strip()
+if opsys == "Darwin":
+    ray_binary = "Contents/MacOS/Ray-UI"
+else:
+    ray_binary = "rayui.sh"
